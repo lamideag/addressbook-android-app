663a7a266e17233382aea3f60b73453023b917da
Nikita Schneider <komesergey@gmail.com>
2022-12-28 23:18:04 +0300

    Basic architecture

--- app/src/main/java/com/deepschneider/addressbook/MainActivity.kt
+++ app/src/main/java/com/deepschneider/addressbook/MainActivity.kt
@@ -3,11 +3,8 @@ package com.deepschneider.addressbook
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.view.Window
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import com.deepschneider.addressbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +13,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        WindowInsetsControllerCompat(window, window.decorView).apply { hide(WindowInsetsCompat.Type.statusBars()) }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         findViewById<Button>(R.id.login_button).setOnClickListener { _ ->
