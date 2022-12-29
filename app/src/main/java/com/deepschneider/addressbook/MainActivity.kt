7301eadd3e8b53559447b1830ba964fd71a39846
Nikita <komesergey@gmail.com>
2022-12-29 22:46:20 +0300

    Basic architecture

--- app/src/main/java/com/deepschneider/addressbook/MainActivity.kt
+++ app/src/main/java/com/deepschneider/addressbook/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.deepschneider.addressbook
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.deepschneider.addressbook.databinding.ActivityMainBinding
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#FFFFFF")))
+        supportActionBar?.elevation = 0F;
         findViewById<Button>(R.id.login_button).setOnClickListener { _ ->
             val intent = Intent(applicationContext, OrganizationActivity::class.java)
             intent.putExtra("message_key", "test")
@@ -25,6 +30,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        return false
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(applicationContext, SettingsActivity::class.java))
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/deepschneider/addressbook/OrganizationActivity.kt
+++ app/src/main/java/com/deepschneider/addressbook/OrganizationActivity.kt
@@ -1,10 +1,13 @@
 package com.deepschneider.addressbook
 
+import android.app.AlertDialog
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -12,10 +15,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.preference.PreferenceManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrganizationActivity : AppCompatActivity() {
 
     private lateinit var toggle: ActionBarDrawerToggle
+
+    private lateinit var searchEditTextLastUpdated: EditText
+
+    private lateinit var searchEditTextType: EditText
+
+    private val lastUpdatedCalendar: Calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +42,8 @@ class OrganizationActivity : AppCompatActivity() {
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        val listView = findViewById<ListView>(R.id.organizationsListView)
-        listView.adapter = ArrayAdapter(
+        val organizationsListView = findViewById<ListView>(R.id.organizationsListView)
+        organizationsListView.adapter = ArrayAdapter(
             this, android.R.layout.simple_list_item_1, arrayOf(
                 "Apple", "Microsoft", "IBM", "Oracle", "Red Hat",
                 "Citibank", "Netflix", "Nvidia", "Intel", "AMD",
@@ -40,28 +51,64 @@ class OrganizationActivity : AppCompatActivity() {
             )
         )
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             startActivity(Intent(applicationContext, CreateNewOrganization::class.java))
         }
+        searchEditTextLastUpdated = findViewById(R.id.searchEditTextLastUpdated)
+        searchEditTextLastUpdated.setOnClickListener {
+            var isDataSet = false
+            val dataPickerDialog = DatePickerDialog(
+                this@OrganizationActivity,
+                { _, year, month, day ->
+                    lastUpdatedCalendar.set(Calendar.YEAR, year)
+                    lastUpdatedCalendar.set(Calendar.MONTH, month)
+                    lastUpdatedCalendar.set(Calendar.DAY_OF_MONTH, day)
+                    updateLabel()
+                    isDataSet = true
+                },
+                lastUpdatedCalendar[Calendar.YEAR],
+                lastUpdatedCalendar[Calendar.MONTH],
+                lastUpdatedCalendar[Calendar.DAY_OF_MONTH]
+            )
+            dataPickerDialog.setOnDismissListener {
+                if (!isDataSet) searchEditTextLastUpdated.setText(null)
+            }
+            dataPickerDialog.show()
+        }
+        searchEditTextType = findViewById(R.id.searchEditTextType)
+        searchEditTextType.setOnClickListener {
+            val builder = AlertDialog.Builder(this@OrganizationActivity)
+            builder.setTitle(R.string.choose_organization_type)
+                .setItems(
+                    R.array.org_types
+                ) { dialog, which ->
+                    if (which == 0)
+                        searchEditTextType.setText(null)
+                    else
+                        searchEditTextType.setText(resources.getStringArray(R.array.org_types)[which])
+                    dialog.dismiss()
+                }
+            builder.create().show()
+        }
+    }
+
+    private fun updateLabel() {
+        searchEditTextLastUpdated.setText(
+            SimpleDateFormat(
+                "MM/dd/yy",
+                Locale.US
+            ).format(lastUpdatedCalendar.time)
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+        return false
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) return true
-        return when (item.itemId) {
-            R.id.action_settings -> {
-                startActivity(Intent(applicationContext, SettingsActivity::class.java))
-                return true
-            }
-            R.id.action_logout -> {
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onResume() {
