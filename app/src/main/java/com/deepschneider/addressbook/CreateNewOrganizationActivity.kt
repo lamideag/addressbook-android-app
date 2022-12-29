233d83cd9450101515a26fe31c71c23afe57de9e
Nikita <komesergey@gmail.com>
2022-12-30 02:35:11 +0300

    Basic architecture

--- app/src/main/java/com/deepschneider/addressbook/CreateNewOrganizationActivity.kt
+++ app/src/main/java/com/deepschneider/addressbook/CreateNewOrganizationActivity.kt
@@ -2,10 +2,23 @@ package com.deepschneider.addressbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class CreateNewOrganizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_organization)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
