633aa1abadff2f6782537fe3d41c0492be7d450c
Nikita <komesergey@gmail.com>
2023-01-05 02:48:00 +0300

    Basic architecture

--- app/src/main/java/com/deepschneider/addressbook/activities/CreateOrEditOrganizationActivity.kt
+++ app/src/main/java/com/deepschneider/addressbook/activities/CreateOrEditOrganizationActivity.kt
@@ -4,7 +4,10 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import com.deepschneider.addressbook.R
 import com.deepschneider.addressbook.dto.OrganizationDto
@@ -13,6 +16,7 @@ import com.deepschneider.addressbook.network.SaveOrCreateEntityRequest
 import com.deepschneider.addressbook.utils.Constants
 import com.deepschneider.addressbook.utils.Urls
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.gson.reflect.TypeToken
 import java.util.UUID
 import java.util.concurrent.ExecutorService
@@ -23,12 +27,84 @@ class CreateOrEditOrganizationActivity : AbstractEntityActivity() {
     private var organizationDto: OrganizationDto? = null
 
     private lateinit var typeEditText: TextInputEditText
+    private lateinit var typeEditTextLayout: TextInputLayout
+
     private lateinit var zipEditText: TextInputEditText
+    private lateinit var zipEditTextLayout: TextInputLayout
+
     private lateinit var addressEditText: TextInputEditText
+    private lateinit var addressEditTextLayout: TextInputLayout
+
     private lateinit var nameEditText: TextInputEditText
+    private lateinit var nameEditTextLayout: TextInputLayout
+
     private lateinit var idEditText: TextInputEditText
     private lateinit var lastUpdatedEditText: TextInputEditText
     private lateinit var saveOrCreateButton: Button
+
+    private val fieldValidation = BooleanArray(4)
+
+    inner class TextFieldValidation(private val view: View) : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            when (view.id) {
+                R.id.create_or_edit_organization_activity_zip -> {
+                    validateZipEditText()
+                }
+                R.id.create_or_edit_organization_activity_address -> {
+                    validateAddressEditText()
+                }
+                R.id.create_or_edit_organization_activity_type -> {
+                    validateTypeEditText()
+                }
+                R.id.create_or_edit_organization_activity_name -> {
+                    validateNameEditText()
+                }
+            }
+            updateSaveButtonState()
+        }
+    }
+
+    private fun validateNameEditText() {
+        if (nameEditText.text.toString().trim().isEmpty()) {
+            nameEditTextLayout.error = "Required Field!"
+            fieldValidation[3] = false
+        } else {
+            nameEditTextLayout.error = null
+            fieldValidation[3] = true
+        }
+    }
+
+    private fun validateAddressEditText() {
+        if (addressEditText.text.toString().trim().isEmpty()) {
+            addressEditTextLayout.error = "Required Field!"
+            fieldValidation[1] = false
+        } else {
+            addressEditTextLayout.error = null
+            fieldValidation[1] = true
+        }
+    }
+
+    private fun validateZipEditText() {
+        if (zipEditText.text.toString().trim().isEmpty()) {
+            zipEditTextLayout.error = "Required Field!"
+            fieldValidation[0] = false
+        } else {
+            zipEditTextLayout.error = null
+            fieldValidation[0] = true
+        }
+    }
+
+    private fun validateTypeEditText() {
+        if (typeEditText.text.toString().trim().isEmpty()) {
+            typeEditTextLayout.error = "Required Field!"
+            fieldValidation[2] = false
+        } else {
+            typeEditTextLayout.error = null
+            fieldValidation[2] = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,11 +119,21 @@ class CreateOrEditOrganizationActivity : AbstractEntityActivity() {
             }
         }
         typeEditText = findViewById(R.id.create_or_edit_organization_activity_type)
+        typeEditTextLayout = findViewById(R.id.create_or_edit_organization_activity_type_layout)
+
         zipEditText = findViewById(R.id.create_or_edit_organization_activity_zip)
+        zipEditTextLayout = findViewById(R.id.create_or_edit_organization_activity_zip_layout)
+
         addressEditText = findViewById(R.id.create_or_edit_organization_activity_address)
+        addressEditTextLayout =
+            findViewById(R.id.create_or_edit_organization_activity_address_layout)
+
         nameEditText = findViewById(R.id.create_or_edit_organization_activity_name)
+        nameEditTextLayout = findViewById(R.id.create_or_edit_organization_activity_name_layout)
+
         idEditText = findViewById(R.id.create_or_edit_organization_activity_id)
         lastUpdatedEditText = findViewById(R.id.create_or_edit_organization_activity_last_updated)
+
         saveOrCreateButton =
             findViewById(R.id.create_or_edit_organization_activity_save_create_button)
         saveOrCreateButton.setOnClickListener {
@@ -65,6 +151,19 @@ class CreateOrEditOrganizationActivity : AbstractEntityActivity() {
             builder.create().show()
         }
         updateUi(organizationDto)
+        setupListeners()
+        validateTypeEditText()
+        validateZipEditText()
+        validateAddressEditText()
+        validateNameEditText()
+        updateSaveButtonState()
+    }
+
+    private fun setupListeners() {
+        typeEditText.addTextChangedListener(TextFieldValidation(typeEditText))
+        zipEditText.addTextChangedListener(TextFieldValidation(zipEditText))
+        addressEditText.addTextChangedListener(TextFieldValidation(addressEditText))
+        nameEditText.addTextChangedListener(TextFieldValidation(nameEditText))
     }
 
     private fun updateUi(organizationDto: OrganizationDto?) {
@@ -87,6 +186,10 @@ class CreateOrEditOrganizationActivity : AbstractEntityActivity() {
 
     private fun convertIndexToType(type: String): String {
         return this.resources.getStringArray(R.array.org_types)[type.toInt() + 1]
+    }
+
+    private fun updateSaveButtonState() {
+        saveOrCreateButton.isEnabled = fieldValidation.all { it }
     }
 
     private fun saveOrCreateOrganization() {
@@ -131,7 +234,7 @@ class CreateOrEditOrganizationActivity : AbstractEntityActivity() {
                                                 Constants.ORGANIZATIONS_CACHE_NAME,
                                                 it
                                             ) else {
-                                                makeSnackBar("Changes saved!")
+                                            makeSnackBar("Changes saved!")
                                         }
                                     }
                                 }
