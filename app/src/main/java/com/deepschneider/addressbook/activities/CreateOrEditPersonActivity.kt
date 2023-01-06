6105c09288d167e7ebd18dabf8892bcae8bd0ec1
Nikita <komesergey@gmail.com>
2023-01-06 15:37:20 +0300

    Basic architecture

--- app/src/main/java/com/deepschneider/addressbook/activities/CreateOrEditPersonActivity.kt
+++ app/src/main/java/com/deepschneider/addressbook/activities/CreateOrEditPersonActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.deepschneider.addressbook.R
+import org.wordpress.aztec.Aztec
+import org.wordpress.aztec.AztecText
+import org.wordpress.aztec.ITextFormat
+import org.wordpress.aztec.source.SourceViewEditText
+import org.wordpress.aztec.toolbar.AztecToolbar
+import org.wordpress.aztec.toolbar.IAztecToolbarClickListener
 
-class CreateOrEditPersonActivity : AppCompatActivity() {
+class CreateOrEditPersonActivity : AppCompatActivity(), IAztecToolbarClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_person)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
-
+        val visualEditor = findViewById<AztecText>(R.id.visual)
+        val sourceEditor = findViewById<SourceViewEditText>(R.id.source)
+        val toolbar = findViewById<AztecToolbar>(R.id.formatting_toolbar)
+        Aztec.with(visualEditor, sourceEditor, toolbar, this)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -22,5 +31,33 @@ class CreateOrEditPersonActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onToolbarCollapseButtonClicked() {
+
+    }
+
+    override fun onToolbarExpandButtonClicked() {
+
+    }
+
+    override fun onToolbarFormatButtonClicked(format: ITextFormat, isKeyboardShortcut: Boolean) {
+
+    }
+
+    override fun onToolbarHeadingButtonClicked() {
+
+    }
+
+    override fun onToolbarHtmlButtonClicked() {
+
+    }
+
+    override fun onToolbarListButtonClicked() {
+
+    }
+
+    override fun onToolbarMediaButtonClicked(): Boolean {
+        return false
     }
 }
